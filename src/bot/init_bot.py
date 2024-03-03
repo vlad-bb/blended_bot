37b34d4fa780f5ef5c6c3e0dd9827a65d335bb31
Vladyslav Babenko <96999713+vlad-bb@users.noreply.github.com>
2024-03-03 12:41:40 +0200

    fix bug

--- src/bot/init_bot.py
+++ src/bot/init_bot.py
@@ -1,3 +1,4 @@
+import os
 from aiogram import Bot, Dispatcher
 from dotenv import dotenv_values
 from aiogram.enums import ParseMode
@@ -6,7 +7,8 @@ from aiogram.enums import ParseMode
 config = dotenv_values(".env")
 
 # Bot token can be obtained via https://t.me/BotFather
-TOKEN = config.get("BOT_TOKEN")
+
+TOKEN = config.get("BOT_TOKEN") if config.get("BOT_TOKEN") else os.environ.get("BOT_TOKEN")
 
 # All handlers should be attached to the Router (or Dispatcher)
 bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
--- src/database/connect_db.py
+++ src/database/connect_db.py
@@ -1,13 +1,14 @@
+import os
 from mongoengine import connect
 from dotenv import dotenv_values
 
 
 config = dotenv_values(".env")
 
-mongo_user = config.get('user')
-mongodb_pass = config.get('pass')
-db_name = config.get('db_name')
-domain = config.get('domain')
+mongo_user = config.get('user') if config.get('user') else os.environ.get("user")
+mongodb_pass = config.get('pass') if config.get('pass') else os.environ.get("pass")
+db_name = config.get('db_name') if config.get('db_name') else os.environ.get("db_name")
+domain = config.get('domain') if config.get('domain') else os.environ.get("domain")
 
 """
 user=admin
